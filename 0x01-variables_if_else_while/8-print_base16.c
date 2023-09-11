fdb5ffc7623cca868c1112365ab7405f9552d50b
Awab isam <awabisam395@gmail.com>
2023-09-11 05:13:36 -0700

    try so hard

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+	char m;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	for (m = 'a' ; m < 'g' ; m++)
+		putchar(m);
+	putchar('\n');
+	return (0);
+}
