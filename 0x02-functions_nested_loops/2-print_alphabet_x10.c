0d67a54f7eed413b85edeadc25b5c672428e34eb
Awab isam <awabisam395@gmail.com>
2023-09-12 13:53:36 -0700

    new test

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+*/
+int main(void)
+{
+	int i;
+	char j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			putchar(j);
+		}
+	putchar('\n');
+	}
+	return (0);
+}
