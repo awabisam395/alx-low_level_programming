55ec75fefac09896422c9c670b7b26522838496d
Awab isam <awabisam395@gmail.com>
2023-09-19 00:15:14 -0700

    try to code

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swap_int - swaps the value of two ints
+ *
+ * Returns: nothing
+ * @a: int to swap
+ * @b: int to swap
+ */
+void swap_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}
