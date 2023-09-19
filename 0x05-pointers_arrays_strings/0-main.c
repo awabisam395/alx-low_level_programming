bf158dfb7f1aa78f3f8c4f92c3c104c94ba4aefc
Awab isam <awabisam395@gmail.com>
2023-09-18 21:52:17 -0700

    try again

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -1,6 +1,9 @@
 #include "main.h"
+
+void reset_to_98(int *);
+
 /**
- * main - check the code 
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
@@ -9,8 +12,8 @@ int main(void)
 	int n;
 
 	n = 402;
-	printf("n=%d\n", n);
+	printf("%d\n", n);
 	reset_to98(&n);
-	printf("n=%d\n", n);	
+	printf("%d\n", n);
 	return (0);
 }
