21fc1f1f4f496768d3872f3e1c4b5e65610cf7b5
Awab isam <awabisam395@gmail.com>
2023-09-21 05:04:25 -0700

    1-strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strncat - used to conatenate two strings
+ * using at most bytes from src
+ * @dest: input val
+ * @src: input val
+ * @n: input val
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int  *n)
+{
+	int r;
+	int s;
+
+	r = 0;
+	while (dest[r] != '\0')
+	{
+		r++;
+	}
+	s = 0;
+	while (r < n && src[s] != '\0')
+	{
+		dest[r] = src[s];
+		r++;
+		s++;
+	}
+	dest[r] = '\0';
+	return (dest);
+}
