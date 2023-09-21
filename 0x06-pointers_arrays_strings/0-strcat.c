fcc38c0b7d8e75d5b22a10105a92c37fdc43ab61
Awab isam <awabisam395@gmail.com>
2023-09-21 04:18:41 -0700

    0-strcat test

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: input val
+ * @src: input val
+ * Return: void
+ */
+chat_strcat(char *dest, char *src)
+{
+	int m;
+	int n;
+
+	m = 0;
+	while (dest[m] != '\0')
+	{
+		m++;
+	}
+	n = 0;
+	while (src[n] != '\0')
+	{
+		dest[m] = src[n];
+		m++;
+		n++;
+	}
+	dest[m] = '\0';
+	return (dest);
+}
